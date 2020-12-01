72aba0fea9ecda9fe67162dc01826d58cfc46898
Kai Niebes <kai.niebes@outlook.com>
2020-12-01 07:17:11 +0100

    Day 01 - Solution with Hash

--- aoc01.cr
+++ aoc01.cr
@@ -4,12 +4,16 @@ require "benchmark"
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt").map(&.to_i)
 
+SUMS = INPUT.combinations(2).to_h { |nums| {nums.sum, nums.product} }
+
 def part1
-  INPUT.combinations(2).select { |nums| nums.sum == 2020 }.first.product
+  SUMS[2020]
 end
 
 def part2
-  INPUT.combinations(3).select { |nums| nums.sum == 2020 }.first.product
+  SUMS.each { |sum, product| INPUT.each { |num|
+    return product * num if sum + num == 2020
+  } }
 end
 
 puts "Part 1\t" + Benchmark.realtime { puts part1 }.to_s
