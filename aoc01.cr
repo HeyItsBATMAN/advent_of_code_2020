80cc8bd031a363347f15bac68c396651826e99cb
Kai Niebes <kai.niebes@outlook.com>
2020-12-01 17:54:25 +0100

    Allow get_input to get different days/years

--- aoc01.cr
+++ aoc01.cr
@@ -1,6 +1,5 @@
 require "benchmark"
 
-# Read real input from file
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt").map(&.to_i)
 
--- get_input.cr
+++ get_input.cr
@@ -1,26 +1,31 @@
 require "http/client"
 
-time = Time.local Time::Location.load("Europe/Berlin")
-year = time.year
-day = time.day
+current_time = Time.local Time::Location.load("Europe/Berlin")
+options = {"day" => current_time.day.to_s, "year" => current_time.year.to_s}
+options.merge! ARGV.map(&.split("=")).to_h
 
 def pad_zero(value : String | Int)
   "00#{value}".split("").last(2).join("")
 end
 
-headers = HTTP::Headers{"cookie" => "session=#{ENV["AOC_SESSION"]}"}
-while true
-  response = HTTP::Client.get \
-    "https://adventofcode.com/#{year}/day/#{day}/input",
-    headers: headers
-  if response.status_code == 200
-    filename = "aoc#{pad_zero(day)}.txt"
-    File.write(filename, response.body)
-    puts "Response written to #{filename}. Good luck!"
-    exit
-  else
-    puts "Failed getting input..."
-    puts response.status_code
+def download_input(day, year)
+  puts "Downloading input for day #{day} year #{year}"
+  headers = HTTP::Headers{"cookie" => "session=#{ENV["AOC_SESSION"]}"}
+  while true
+    response = HTTP::Client.get \
+      "https://adventofcode.com/#{year}/day/#{day}/input",
+      headers: headers
+    if response.status_code == 200
+      filename = "aoc#{pad_zero(day)}.txt"
+      File.write(filename, response.body)
+      puts "Response written to #{filename}. Good luck!"
+      break
+    else
+      puts "Failed getting input..."
+      puts response.status_code
+    end
+    sleep 15
   end
-  sleep 15
 end
+
+download_input day: options["day"], year: options["year"]
