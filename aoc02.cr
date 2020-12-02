1bce14b51602cd763034913122863494068e51c1
Kai Niebes <kai.niebes@outlook.com>
2020-12-02 08:30:21 +0100

    Day 02 - Pretty optimized solution

--- aoc02.cr
+++ aoc02.cr
@@ -1,25 +1,17 @@
 require "benchmark"
 
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
-INPUT = File.read_lines("#{DAY}.txt").map { |line|
-  m = line.match(/(\d+)-(\d+) (\w): (\w+)/).not_nil!
-  {password: m[4], letter: m[3][0], nums: [m[1], m[2]].map(&.to_i)}
-}
+INPUT = File.read_lines("#{DAY}.txt")
+total = Benchmark.realtime do
+  silver, gold = 0, 0
+  INPUT.each do |line|
+    low, high, letter, password = line.sub("-", " ").split
+    low, high, letter = low.to_i, high.to_i, letter[0]
+    count = password.count letter
 
-def part1
-  INPUT.select { |pw|
-    (pw[:nums].first..pw[:nums].last).covers? pw[:password].count pw[:letter]
-  }.size
+    silver += 1 if count >= low && count <= high
+    gold += 1 if (password[low - 1] == letter) ^ (password[high - 1] == letter)
+  end
+  puts "Silver: #{silver}\tGold: #{gold}"
 end
-
-def part2
-  INPUT.select { |pw|
-    result = pw[:nums].map { |pos| pw[:password][pos - 1] == pw[:letter] }.uniq
-    result.size == 2 && result.find { |b| b }
-  }.size
-end
-
-part1time = Benchmark.realtime { puts part1 }.total_milliseconds
-puts "Part 1\t#{part1time}ms"
-part2time = Benchmark.realtime { puts part2 }.total_milliseconds
-puts "Part 2\t#{part2time}ms"
+puts "Time:#{total.total_milliseconds}ms"
