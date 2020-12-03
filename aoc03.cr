a699abf69157f818819ec6ab5ee88ef37bffb9c0
Kai Niebes <kai.niebes@outlook.com>
2020-12-03 15:43:15 +0100

    Day 03 - Fix order

--- aoc03.cr
+++ aoc03.cr
@@ -2,15 +2,16 @@ DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt")
 
 SLOPES = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-TREES  = [0, 0, 0, 0, 0]
+TREES  = Array.new(SLOPES.size) { 0 }
 
-width = INPUT.first.size
-INPUT.each_with_index do |line, line_index|
-  SLOPES.each_with_index do |slope, slope_index|
-    x, y = slope
-    next if line_index % y != 0
-    tree = line[(x * line_index) % width]
-    TREES[slope_index] += 1 if tree == '#'
+width, height = INPUT.first.size, INPUT.size
+SLOPES.each_with_index do |slope, slope_index|
+  right, down = slope
+  x, y = 0, 0
+  until y >= height
+    TREES[slope_index] += 1 if INPUT[y][x % width] == '#'
+    x += right
+    y += down
   end
 end
 
