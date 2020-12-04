523e5fd1e94723150c2d279deb0a3858c7d71c1c
Kai Niebes <kai.niebes@outlook.com>
2020-12-04 23:44:58 +0100

    Day 04 - Save some iterations by looking for the first non valid entry

--- aoc04.cr
+++ aoc04.cr
@@ -1,8 +1,7 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read("#{DAY}.txt")
-  .split("\n\n")                 # seperate by passport
-  .map(&.split.flatten)          # make even
-  .map(&.map(&.split(":")).to_h) # as hash
+  .split("\n\n")                               # seperate by passport
+  .map(&.split.flatten.map(&.split(":")).to_h) # as hash
 
 REQS       = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"]
 EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
@@ -14,23 +13,21 @@ def is_valid(passport : Passport)
 end
 
 def is_strict_valid(passport : Passport)
-  7 == passport.map { |key, value|
+  passport.find { |key, value|
     case key
-    when "byr" then (1920..2002).covers? value.to_i
-    when "iyr" then (2010..2020).covers? value.to_i
-    when "eyr" then (2020..2030).covers? value.to_i
+    when "byr" then !("1920".."2002").covers? value
+    when "iyr" then !("2010".."2020").covers? value
+    when "eyr" then !("2020".."2030").covers? value
     when "hgt"
       m = value.match(/^(\d+)(cm|in)$/)
       return false if !m
-      h, u = m[1].to_i, m[2]
-      u == "cm" ? (150..193).covers?(h) : (59..76).covers?(h)
-    when "hcl" then !value.match(/^#([0-9a-f]{6})$/).nil?
-    when "ecl" then EYE_COLORS.includes?(value)
-    when "pid" then value.to_i? && value.size == 9
-    when "cid" then false
+      m[2] == "cm" ? !("150".."193").covers?(m[1]) : !("59".."76").covers?(m[1])
+    when "hcl" then value.match(/^#([0-9a-f]{6})$/).nil?
+    when "ecl" then !EYE_COLORS.includes?(value)
+    when "pid" then !value.to_i? || value.size != 9
     else            false
     end
-  }.count(true)
+  }.nil?
 end
 
 valid = INPUT.select(->is_valid(Passport))
