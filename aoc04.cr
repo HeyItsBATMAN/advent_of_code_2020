564bbe4f47ceb967f5883f1af176bbf35ffa74b4
Kai Niebes <kai.niebes@outlook.com>
2020-12-04 11:23:54 +0100

    Day 04 - Improve input processing

--- aoc04.cr
+++ aoc04.cr
@@ -1,15 +1,20 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
-INPUT = File.read_lines("#{DAY}.txt")
+INPUT = File.read("#{DAY}.txt")
+  .split("\n\n")                 # seperate by passport
+  .map(&.split.flatten)          # make even
+  .map(&.map(&.split(":")).to_h) # as hash
 
 REQS       = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"]
 EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
-def is_valid(passport : Hash(String, String))
+alias Passport = Hash(String, String)
+
+def is_valid(passport : Passport)
   !REQS.find { |key| !passport[key]? }
 end
 
-def is_strict_valid(passport : Hash(String, String))
-  count = passport.map { |key, value|
+def is_strict_valid(passport : Passport)
+  7 == passport.map { |key, value|
     case key
     when "byr" then (1920..2002).covers? value.to_i
     when "iyr" then (2010..2020).covers? value.to_i
@@ -21,30 +26,15 @@ def is_strict_valid(passport : Hash(String, String))
       u == "cm" ? (150..193).covers?(h) : (59..76).covers?(h)
     when "hcl" then !value.match(/^#([0-9a-f]{6})$/).nil?
     when "ecl" then EYE_COLORS.includes?(value)
-    when "pid" then !value.match(/^([0-9]{9})$/).nil?
-    when "cid" then true
+    when "pid" then value.to_i? && value.size == 9
+    when "cid" then false
     else            false
     end
   }.count(true)
-
-  passport["cid"]? ? count == 8 : count == 7
 end
 
-valid = 0
-strict = 0
-passport = Hash(String, String).new
-INPUT.each_with_index do |line, line_index|
-  if line.empty? || line_index + 1 >= INPUT.size
-    valid += 1 if is_valid(passport)
-    strict += 1 if is_strict_valid(passport)
-    passport.clear
-  else
-    line.split.each do |s|
-      key, value = s.split(":") #
-      passport[key] = value
-    end
-  end
-end
+valid = INPUT.select(->is_valid(Passport))
+strict = valid.select(->is_strict_valid(Passport))
 
-puts valid
-puts strict
+puts valid.size
+puts strict.size
