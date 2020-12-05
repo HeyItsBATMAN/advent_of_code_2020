a733ddc8afc5c1e69099485f7c39be49f51664cb
Kai Niebes <kai.niebes@outlook.com>
2020-12-05 13:43:39 +0100

    Day 05 - Silly optimizations

--- aoc05.cr
+++ aoc05.cr
@@ -1,12 +1,19 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt")
-  .map(&.gsub("F", "0").gsub("B", 1).gsub("L", 0).gsub("R", 1).to_i(2))
-  .sort
+SEATS = Set(Int32).new initial_capacity: INPUT.size
+
+max_seat = 0
+INPUT.each { |line|
+  seat_id = line.tr("BRFL", "1100").to_i(2)
+  max_seat = seat_id if seat_id > max_seat
+  SEATS << seat_id
+}
 
 # Part 1
-puts INPUT.last
+puts max_seat
 
 # Part 2
-puts (0..INPUT.last).find { |val|
-  (val - 1..val + 1).to_a.map { |v| INPUT.includes?(v) } == [true, false, true]
+puts (0..max_seat).find { |val|
+  next if SEATS.includes? val
+  SEATS.includes?(val + 1) && SEATS.includes?(val - 1)
 }
