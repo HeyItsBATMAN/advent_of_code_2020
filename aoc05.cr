9fb8ef6ed0797a3ee7363823d4a498559a074ef8
Kai Niebes <kai.niebes@outlook.com>
2020-12-05 21:20:26 +0100

    Day 05 - Final silly optimizations

--- aoc05.cr
+++ aoc05.cr
@@ -1,9 +1,12 @@
 DAY = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 
 min, max, sum, seat_id = UInt64::MAX, 0_u64, 0_u64, 0_u64
-
 File.read_lines("#{DAY}.txt").each { |line|
-  seat_id = line.tr("BRFL", "1100").to_u64(2)
+  seat_id = 0_u64
+  line.each_char_with_index { |c, i|
+    next if c == 'F' || c == 'L'
+    seat_id += (2 ** (line.size - i - 1))
+  }
   sum += seat_id
   min = seat_id if seat_id < min
   max = seat_id if seat_id > max
