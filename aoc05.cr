c009baebb301d7bf1ce8cd85315a7d20ab7bd886
Kai Niebes <kai.niebes@outlook.com>
2020-12-05 06:49:07 +0100

    Day 05 - Take each line as binary representation of seatID

--- aoc05.cr
+++ aoc05.cr
@@ -1,37 +1,12 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt")
-
-seats = Array(Tuple(Int32, Int32)).new
-INPUT.each do |line|
-  y, x = (0..127), (0..7)
-  ysize, xsize = 64, 4
-  line.each_char do |char|
-    case char
-    when 'F'
-      y = y.first(ysize)
-      ysize = ysize.tdiv(2)
-    when 'B'
-      y = y.skip(ysize)
-      ysize = ysize.tdiv(2)
-    when 'L'
-      x = x.first(xsize)
-      xsize = xsize.tdiv(2)
-    when 'R'
-      x = x.skip(xsize)
-      xsize = xsize.tdiv(2)
-    end
-  end
-  seats << {y.first, x.first}
-end
+  .map(&.gsub("F", "0").gsub("B", 1).gsub("L", 0).gsub("R", 1).to_i(2))
+  .sort
 
 # Part 1
-puts seats.max_of { |seat| seat.first * 8 + seat.last }
+puts INPUT.last
 
 # Part 2
-(15..112).each { |y| (0..7).each { |x|
-  res = seats.find { |seat| seat == {y, x} }
-  if !res
-    puts y * 8 + x
-    exit
-  end
-} }
+puts (0..INPUT.last).find { |val|
+  (val - 1..val + 1).to_a.map { |v| INPUT.includes?(v) } == [true, false, true]
+}
