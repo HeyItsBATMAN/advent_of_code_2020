d4d1cec98211e18e96ce8d1aeee890ea6f1002fe
Kai Niebes <kai.niebes@outlook.com>
2020-12-06 09:17:34 +0100

    Day 06 - More golfing

--- aoc06.cr
+++ aoc06.cr
@@ -1,5 +1,5 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read("#{DAY}.txt").split("\n\n").map(&.lines.map(&.chars))
 
-puts INPUT.sum { |group| group.flatten.uniq.size }
-puts INPUT.sum { |group| group.reduce { |s, val| s & val }.size }
+puts INPUT.sum(&.flatten.uniq.size)
+puts INPUT.sum(&.reduce { |s, val| s & val }.size)
