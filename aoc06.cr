7c7cc23e6228156d14faf67825be6bbf97019575
Kai Niebes <kai.niebes@outlook.com>
2020-12-06 09:14:40 +0100

    Day 06 - .lines to seperate groups on \n

--- aoc06.cr
+++ aoc06.cr
@@ -1,8 +1,5 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
-INPUT = File.read("#{DAY}.txt")
-  .split("\n\n")
-  .map(&.split("\n").reject(&.empty?))
-  .map(&.map(&.chars))
+INPUT = File.read("#{DAY}.txt").split("\n\n").map(&.lines.map(&.chars))
 
 puts INPUT.sum { |group| group.flatten.uniq.size }
 puts INPUT.sum { |group| group.reduce { |s, val| s & val }.size }
