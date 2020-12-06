e5741f6c5c55c8a442b0043863fd05ccb25be886
Kai Niebes <kai.niebes@outlook.com>
2020-12-06 06:31:15 +0100

    Day 06 - Better parsing & array reduction

--- aoc06.cr
+++ aoc06.cr
@@ -1,36 +1,15 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
-INPUT = File.read_lines("#{DAY}.txt")
+INPUT = File.read("#{DAY}.txt")
+  .split("\n\n")
+  .map(&.split("\n").reject(&.empty?))
+  .map(&.map(&.chars))
 
 def part1
-  input = INPUT.clone
-  counter = 0
-  group = Set(Char).new
-  input.each do |line|
-    if line.empty?
-      counter += group.size
-      group.clear
-    else
-      group += line.chars.to_set
-    end
-  end
-  counter + group.size
+  INPUT.reduce(0) { |acc, group| acc + group.flatten.uniq.size }
 end
 
 def part2
-  input = INPUT.clone
-  counter = 0
-  group = [] of Set(Char)
-  input.each do |line|
-    if line.empty?
-      solution = group.reduce { |acc, val| acc & val }
-      counter += solution.size
-      group.clear
-    else
-      group << line.chars.to_set
-    end
-  end
-  solution = group.reduce { |acc, val| acc & val }
-  counter + solution.size
+  INPUT.reduce(0) { |acc, group| acc + group.reduce { |s, val| s & val }.size }
 end
 
 puts part1
