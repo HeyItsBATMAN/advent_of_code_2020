d747225052ff537c5ab1f26fe755cc60ea61abc7
Kai Niebes <kai.niebes@outlook.com>
2020-12-07 07:18:57 +0100

    Day 07 - Shortened

--- aoc07.cr
+++ aoc07.cr
@@ -1,55 +1,28 @@
-DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
-INPUT = File.read_lines("#{DAY}.txt")
-
-BAGS = INPUT.map { |line|
+DAY  = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
+BAGS = File.read_lines("#{DAY}.txt").map { |line|
   line = line
-    .gsub("bags contain", ",")
-    .gsub(".", ",")
-    .gsub("no other bags", ",")
-    .split(",")
-    .map(&.strip)
-    .reject(&.empty?)
-  first = line.shift
+    .gsub(/bags*/, "").sub("contain", ",").sub(".", ",").sub("no other", ",")
+    .split(",").map(&.strip).reject(&.empty?)
+  bag_name = line.shift
 
-  line = line
-    .map(&.gsub("bags", "").gsub("bag", "").strip)
-    .map { |bag|
-      split = bag.split(" ")
-      number = split.shift.to_i
-      {number, split.join(" ")}
-    }
+  contained_bags = line.map { |bag|
+    number, name = bag.strip.split limit: 2
+    {amount: number.to_i, name: name}
+  }
 
-  {first, line}
+  {bag_name, contained_bags}
 }.to_h
 
 def holds_shiny(key : String)
   return true if key == "shiny gold"
   current = BAGS[key]?
-  return false if !current
-  return !current.find { |bag| holds_shiny(bag.last) }.nil?
+  current.nil? ? false : !current.find { |bag| holds_shiny(bag[:name]) }.nil?
 end
 
 def bags_required(key : String)
   current = BAGS[key]?
-  return 0 if current.nil?
-  return 1 + current.sum { |bag|
-    mult = bag.first
-    name = bag.last
-    nested = mult * bags_required(name)
-    nested
-  }
+  current.nil? ? 0 : 1 + current.sum { |bag| bag[:amount] * bags_required(bag[:name]) }
 end
 
-def part1
-  BAGS.keys.select { |bag|
-    next if bag == "shiny gold"
-    holds_shiny(bag)
-  }.size
-end
-
-def part2
-  bags_required("shiny gold") - 1
-end
-
-puts part1
-puts part2
+puts BAGS.keys.select { |bag| bag != "shiny gold" && holds_shiny(bag) }.size
+puts bags_required("shiny gold") - 1
