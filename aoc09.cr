c61655c0a77df630f36dc661eb03f41edd7d7733
Kai Niebes <kai.niebes@outlook.com>
2020-12-09 06:33:29 +0100

    Day 09 - Shortened solution

--- aoc09.cr
+++ aoc09.cr
@@ -3,25 +3,21 @@ INPUT         = File.read_lines("#{DAY}.txt").map(&.to_u64)
 PREAMBLE_SIZE = 25
 
 def part1
-  INPUT.each_index do |start_index|
-    preamble = INPUT.skip(start_index).first(PREAMBLE_SIZE + 1)
+  INPUT.each_cons(PREAMBLE_SIZE + 1) do |preamble|
     last = preamble.pop
-    sums = preamble.combinations(2).map(&.sum)
-    return last if !sums.includes?(last)
+    return last if !preamble.combinations(2).find { |nums| nums.sum == last }
   end
 end
 
 def part2
   corrupted = part1 || 0
   input = INPUT.clone.select { |num| num < corrupted }
-  input.each_index do |start_index|
-    (2...input.size).each do |n|
-      preamble = input.skip(start_index).first(n)
-      sum = preamble.sum
-      break if sum > corrupted
-      return preamble.min + preamble.max if sum == corrupted
-    end
-  end
+  input.each_index { |start_index| (2...input.size).each { |n|
+    preamble = input[input.size - start_index, n]
+    sum = preamble.sum
+    break if sum > corrupted
+    return preamble.min + preamble.max if sum == corrupted
+  } }
 end
 
 puts part1
