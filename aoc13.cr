838917f91aded05870cc8921ea21f5254609cbb2
Kai Niebes <kai.niebes@outlook.com>
2020-12-13 09:06:37 +0100

    Day 13 - Shorter solution

--- aoc13.cr
+++ aoc13.cr
@@ -1,53 +1,29 @@
 DAY   = PROGRAM_NAME.match(/aoc\d{2}/).not_nil![0]
 INPUT = File.read_lines("#{DAY}.txt")
 
-EARLY_TIME = INPUT.first.to_i
-LINES      = INPUT.last.split(",")
+DEPARTURE = INPUT.first.to_i
+LINES     = INPUT.last.split(",")
   .reject { |val| val == "x" }
-  .map { |val| {val.to_u64, val.to_u64} }.to_h
+  .map { |val| {val.to_u64, INPUT.last.split(",").index(val).not_nil!} }.to_h
 
-def part1
-  lines = INPUT.last.split(",")
-    .reject { |val| val == "x" }
-    .map { |val| {val.to_u64, val.to_u64} }.to_h
-  until lines.all? { |id, val| val > EARLY_TIME }
-    lines.keys.each do |id|
-      lines[id] += id if lines[id] < EARLY_TIME
-    end
-  end
-  if bus = lines.min_by { |id, val| val }
-    id, minutes = bus.first, bus.last
-    return (minutes - EARLY_TIME) * id
-  end
+def part1(lines = LINES.keys)
+  first = lines.map { |id| {DEPARTURE - (DEPARTURE % id) + id, id} }.to_h.min
+  (first.first - DEPARTURE) * first.last
 end
 
-def part2
-  split = INPUT.last.split(",")
-
-  lines = INPUT.last.split(",")
-    .reject { |val| val == "x" }
-    .map { |val| {val.to_u64, split.index(val).not_nil!} }.to_h
-  ids = lines.keys
-
+def part2(lines = LINES.clone)
   step = [lines.first.first]
   lcm = ->{
-    temp = step.clone
     num = 1_u64
-    until temp.empty?
-      num = num.lcm(temp.pop)
-    end
+    step.size.times { |i| num = num.lcm(step[i]) }
     num
   }
   timestamp = lcm.call
-  until ids.empty?
-    id = ids.shift
-
+  lines.keys.each do |id|
     until (timestamp + lines[id]) % id == 0
       timestamp += lcm.call
     end
-
     step << id
-    step.uniq!
   end
   timestamp
 end
