1b052c061e4221a1fec32e3c44037073a0589a30
Kai Niebes <kai.niebes@outlook.com>
2020-12-20 14:29:04 +0100

    Day 20 - First solution

--- aoc20.cr
+++ aoc20.cr
@@ -3,74 +3,47 @@ INPUT = File.read("#{DAY}.txt").strip.split("\n\n").map(&.split("\n"))
 TILES = Array(Tile).new
 
 class Tile
+  getter id
+  property data
+
   def initialize(@id : Int32, @data : Array(String))
   end
 
-  def_equals_and_hash @id
-  def_clone
-  forward_missing_to @data
-
-  def id
-    @id
-  end
-
   def to_s(io)
-    io << self.id
+    io << "T<#{self.id}>"
   end
 
-  def flip_y
+  def flip
     tile = self.clone
-    tile.reverse!
-    tile
-  end
-
-  def flip_x
-    tile = self.clone
-    tile.map!(&.reverse)
-    tile
-  end
-
-  def rotate_left
-    tile = self.clone
-    row_len = tile.first.size
-    row_len.times do |i|
-      tile[i] = self.map { |row| row[row_len - i - 1] }.join
-    end
+    tile.data = tile.map(&.reverse)
     tile
   end
 
   def rotate_right
     tile = self.clone
-    tile.rotate_left.flip_x.flip_y
-  end
-
-  def top
-    self.first
-  end
-
-  def left
-    self.map { |row| row[0] }.join
-  end
-
-  def bottom
-    self.last
+    tile.data = tile.map(&.chars).transpose.map(&.reverse.join(""))
+    tile
   end
 
   def right
     self.map { |row| row[row.size - 1] }.join
   end
 
-  def sides
-    [self.top, self.right, self.bottom, self.left]
+  def left
+    self.map { |row| row[0] }.join
   end
 
-  def reverse_sides
-    [self.top.reverse, self.right.reverse, self.bottom.reverse, self.left.reverse]
+  def sides
+    [self.first, self.right, self.last, self.left]
   end
 
   def all_sides
-    [self.top, self.right, self.bottom, self.left, self.top.reverse, self.right.reverse, self.bottom.reverse, self.left.reverse]
+    [self.sides, self.sides.map(&.reverse)].flatten
   end
+
+  def_equals_and_hash @id
+  def_clone
+  forward_missing_to @data
 end
 
 INPUT.each do |tile|
@@ -84,4 +57,90 @@ def find_adjacent(tile : Tile)
 end
 
 # Part 1
-puts TILES.select { |t| find_adjacent(t).size == 2 }.map(&.id.to_u64).product
+corners = TILES.select { |t| find_adjacent(t).size == 2 }
+puts corners.map(&.id.to_u64).product
+
+# Part 2
+GRID_SIZE = Math.sqrt(INPUT.size).to_i
+
+def get_orientations(start : Tile)
+  arr = [start.clone]
+  3.times { arr << arr.last.rotate_right }
+  arr << start.clone.flip
+  3.times { arr << arr.last.rotate_right }
+  arr
+end
+
+def find_right_adjacent(tile : Tile, tiles_remaining : Array(Tile))
+  tiles_remaining.each do |other|
+    ori = get_orientations(other).find { |o| tile.right == o.left }
+    return ori if ori
+  end
+end
+
+def build_row(starting_tile : Tile, tiles_remaining = TILES.clone)
+  tiles_remaining.delete(starting_tile)
+  row = [starting_tile]
+  until row.size == GRID_SIZE
+    next_tile = find_right_adjacent(row.last, tiles_remaining) || return
+    tiles_remaining.delete(next_tile)
+    row << next_tile
+  end
+  row
+end
+
+GRID = Array(Array(Tile)).new
+corners.each do |starting_corner|
+  break if !GRID.empty?
+  grid = Array(Array(Tile)).new
+  get_orientations(starting_corner).each do |orientation|
+    break if !GRID.empty?
+    tiles_remaining = TILES.clone
+    grid.clear
+    first_row = build_row(orientation, tiles_remaining)
+    next if !first_row
+    # Rotate first row left
+    grid << first_row.map(&.rotate_right)
+    grid = grid.transpose
+
+    # Build remaining rows
+    restart = false
+    grid.each do |row|
+      full_row = build_row(row.first, tiles_remaining)
+      if !full_row
+        restart = true
+        break
+      end
+      GRID << full_row
+    end
+    GRID.clear if restart
+  end
+end
+
+# Remove borders
+grid = GRID.map(&.map(&.data.rotate(-1).skip(2).map(&.lchop.rchop)))
+lines = Array(String).new
+grid.each { |tr| tr.first.size.times { |i| lines << tr.map { |t| t[i] }.join } }
+
+# Generate map orientations
+oris = [lines]
+3.times { oris << oris.last.map(&.chars).transpose.map(&.reverse.join("")) }
+oris << lines.clone.map(&.reverse)
+3.times { oris << oris.last.map(&.chars).transpose.map(&.reverse.join("")) }
+
+# Find monster in orientations
+FINE_MATCH = /(.{18}#.\n#.{4}##.{4}##.{4}###\n.#.{2}#.{2}#.{2}#.{2}#.{2}#.{3})/
+monster_size, monster_width = 15, 20
+
+oris.each do |ori|
+  monster_count = 0
+  ori.each_cons(3) do |group|
+    (0..group.first.size - monster_width).each do |i|
+      smaller_group = group.map { |line| line[i, monster_width] }.join("\n")
+      monster_count += 1 if smaller_group.matches?(FINE_MATCH)
+    end
+  end
+  next if monster_count == 0
+  puts ori.join("\n").count('#') - (monster_count * monster_size)
+  break
+end
