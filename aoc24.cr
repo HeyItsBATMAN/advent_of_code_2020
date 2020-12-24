9a3c8187b7c15f3215b0b000ef72cd8f9c4994e7
Kai Niebes <kai.niebes@outlook.com>
2020-12-24 07:11:42 +0100

    Day 24 - Shorter solution

--- aoc24.cr
+++ aoc24.cr
@@ -8,33 +8,19 @@ def add_pos(a, b)
 end
 
 def initial_flipping
-  input = INPUT.clone
   movements = {"nw" => {0, -1}, "w" => {-1, -1}, "sw" => {-1, 0},
                "ne" => {1, 0}, "e" => {1, 1}, "se" => {0, 1}}
-
   tiles = Hash(Pos, Bool).new
-
   start_pos = {0, 0}
-  input.each do |line|
+  INPUT.each do |line|
     pos = start_pos.clone
     until line.empty?
       token = line[0, 2]
-      dir = ["se", "sw", "nw", "ne"].find { |d| d == token }
-      if !dir
-        dir = token[0].to_s
-        line = line.lchop
-      else
-        2.times { line = line.lchop }
-      end
-
-      mvmt = movements[dir]
-      pos = add_pos(pos, mvmt)
-
+      dir = ["se", "sw", "nw", "ne"].find { |d| d == token } || token[0].to_s
+      dir.size.times { line = line.lchop }
+      pos = add_pos(pos, movements[dir])
       tiles[pos] = true if tiles[pos]? == nil
-      if line.empty?
-        val = tiles[pos]?
-        tiles[pos] = val == nil ? false : !tiles[pos]
-      end
+      tiles[pos] = tiles[pos]? == nil ? false : !tiles[pos] if line.empty?
     end
   end
   tiles
@@ -47,34 +33,23 @@ end
 def part2
   movements = [{0, -1}, {-1, -1}, {-1, 0}, {1, 0}, {1, 1}, {0, 1}]
   tiles = initial_flipping
-
   100.times do
     next_tiles = Hash(Pos, Bool).new
     # expand map
-    tiles.each do |pos, _|
-      movements.each { |mvmt|
-        new_pos = add_pos(pos, mvmt)
-        tiles[new_pos] = true if tiles[new_pos]? == nil
-      }
-    end
-
+    tiles.each { |pos, _| movements.each { |m|
+      p = add_pos(pos, m)
+      tiles[p] = true if tiles[p]? == nil
+    } }
     # apply state changes
     tiles.each do |pos, state|
-      adj_black = movements.map { |mvmt|
-        new_pos = add_pos(pos, mvmt)
-        tiles[new_pos] = true if tiles[new_pos]? == nil
-        tiles[new_pos]
+      adj_black = movements.map { |m| add_pos(pos, m) }.map { |p|
+        tiles[p]? == nil ? true : tiles[p]
       }.count(false)
-      case state
-      when true # white
-        next_tiles[pos] = adj_black != 2
-      when false # black
-        next_tiles[pos] = adj_black == 0 || adj_black > 2
-      end
+
+      next_tiles[pos] = state ? adj_black != 2 : adj_black == 0 || adj_black > 2
     end
     tiles = tiles.merge(next_tiles)
   end
-
   tiles.values.count(false)
 end
 
