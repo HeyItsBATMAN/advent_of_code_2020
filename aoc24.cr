2075f3c8404475b6e522bcc09174645798f8316d
Kai Niebes <kai.niebes@outlook.com>
2020-12-24 07:01:39 +0100

    Day 24 - Part 2

--- aoc24.cr
+++ aoc24.cr
@@ -7,7 +7,7 @@ def add_pos(a, b)
   {a.first + b.first, a.last + b.last}
 end
 
-def part1
+def initial_flipping
   input = INPUT.clone
   movements = {"nw" => {0, -1}, "w" => {-1, -1}, "sw" => {-1, 0},
                "ne" => {1, 0}, "e" => {1, 1}, "se" => {0, 1}}
@@ -29,21 +29,54 @@ def part1
 
       mvmt = movements[dir]
       pos = add_pos(pos, mvmt)
+
+      tiles[pos] = true if tiles[pos]? == nil
       if line.empty?
         val = tiles[pos]?
         tiles[pos] = val == nil ? false : !tiles[pos]
       end
     end
   end
-  tiles.values.count(false)
+  tiles
+end
+
+def part1
+  initial_flipping.values.count(false)
 end
 
 def part2
-  input = INPUT.clone
-  input.each do |line|
+  movements = [{0, -1}, {-1, -1}, {-1, 0}, {1, 0}, {1, 1}, {0, 1}]
+  tiles = initial_flipping
+
+  100.times do
+    next_tiles = Hash(Pos, Bool).new
+    # expand map
+    tiles.each do |pos, _|
+      movements.each { |mvmt|
+        new_pos = add_pos(pos, mvmt)
+        tiles[new_pos] = true if tiles[new_pos]? == nil
+      }
+    end
+
+    # apply state changes
+    tiles.each do |pos, state|
+      adj_black = movements.map { |mvmt|
+        new_pos = add_pos(pos, mvmt)
+        tiles[new_pos] = true if tiles[new_pos]? == nil
+        tiles[new_pos]
+      }.count(false)
+      case state
+      when true # white
+        next_tiles[pos] = adj_black != 2
+      when false # black
+        next_tiles[pos] = adj_black == 0 || adj_black > 2
+      end
+    end
+    tiles = tiles.merge(next_tiles)
   end
-  input
+
+  tiles.values.count(false)
 end
 
 puts part1
-# puts part2
+puts part2
